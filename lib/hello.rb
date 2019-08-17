419fac79bc995ac230b526b49914ee79f0305536
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 16:18:00 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,26 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      array[i]
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
+def hello_t(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
