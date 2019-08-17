0b62524cd4dde4a811bef1836986c4513ce41a28
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 16:06:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,10 @@
-def hello_t
-  names.each do |name|
-    puts name
+def hello_t(array)
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i = i + 1
   end
-  return names
 end
 
 # call your method here!
